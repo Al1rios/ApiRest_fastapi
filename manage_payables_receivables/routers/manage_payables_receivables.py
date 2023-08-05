eab29493e0f541860917e9d3490014651a27f9ab
Ali Rios <ali.rios@gmail.com>
2023-08-05 19:25:28 -0300

    added routers

--- manage_payables_receivables/routers/manage_payables_receivables.py
+++ manage_payables_receivables/routers/manage_payables_receivables.py
@@ -1,13 +1,45 @@
+from decimal import Decimal
+from typing import List
+
 from fastapi import APIRouter
-
-
+from pydantic import BaseModel
 
 router = APIRouter(prefix="/manage_payables_receivables")
 
-@router.get("/")
+class ManagePayablesReceivablesResponse(BaseModel):
+    id: int
+    description: str
+    value: Decimal
+    type: str # payables, receivables
+
+class ManagePayablesReceivablesRequest(BaseModel):
+    description: str
+    value: Decimal
+    type: str # payables, receivables
+
+@router.get("/", response_model=List[ManagePayablesReceivablesResponse])
 def list_all_payables_and_receivables():
-    return [{
-        "contas1":"contas1"
-    },{
-        "contas2":"contas2"
-    }]
+    return [
+        ManagePayablesReceivablesResponse(
+            id = 1,
+            description = "rent",
+            value = 2300.50,
+            type = "payables"
+        ),
+        ManagePayablesReceivablesResponse(
+            id=2,
+            description="salary",
+            value=5000.90,
+            type="receivables"
+        ),
+    ]
+
+
+@router.post("/", response_model=ManagePayablesReceivablesResponse, status_code=201)
+def create_account(account: ManagePayablesReceivablesRequest):
+    return ManagePayablesReceivablesResponse(
+            id=3,
+            description=account.description,
+            value=account.value,
+            type=account.type
+        )
