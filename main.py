01c61efbb3071dd1de5d258f30460e624dde1116
Ali Rios <ali.rios@gmail.com>
2023-08-06 20:50:25 -0300

    added documentation openapi

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ from fastapi import FastAPI
 from manage_payables_receivables.routers import manage_payables_receivables as man
 
 
-app = FastAPI()
+app = FastAPI(
+    title="API Testing",
+    description="API for testing with FastAPI",
+    version="1.0.0",
+    openapi_url="/api/v1/openapi.json"  # URL para a especificação OpenAPI
+)
 
 
 @app.get("/")
 def index():
     return {"message": "Hello World"}
+
 
 app.include_router(man.router)
